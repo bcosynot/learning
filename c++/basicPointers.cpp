38cd859129955e8b287404286e99852d740b20ea
Vivek <vr3690@gmail.com>
2014-01-19 23:48:18 +0000

    Starting off with pointers.

---
+++ c++/basicPointers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+/**
+ * This program is an attempt to get a handle
+ * on pointers. The very basic and nothing fancy.
+ * Baby steps, yo.*
+ */
+int main(){
+  // Declare a variable.
+  int a = 23;
+  // Declare a pointer.
+  int* pointerToA = &a;
+  // Let's print both out and see the results.
+  cout << endl << a << "-" << pointerToA << endl;
+}
